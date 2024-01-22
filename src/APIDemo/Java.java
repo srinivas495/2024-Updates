7350aa393c90e47fdca5cb53d602e63d42f4b9ae
Srinivas <Srinivas@NaliTaniLokiSri>
2024-01-22 23:31:59 +0530

    New pro

---
+++ src/APIDemo/Java.java
@@ -0,0 +1,17 @@
+package APIDemo;
+
+public class Java {
+
+	public static void main(String[] args) {
+		String a = "123";
+		int b = 789;
+		
+		System.out.println(a+ b);
+
+	
+				
+		
+
+	}
+
+}
---
+++ src/JavaSessions/DataTypes.java
@@ -0,0 +1,33 @@
+package JavaSessions;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		//Data types
+		// Name String int
+		// Declare the type of the data 
+		//Java is faster Python
+		// Java is Compile time and runtime Python there is no compile time only run time
+		
+		int a = 20;
+		int b = 20;
+				
+		System.out.println("B Value is " + (a+b));
+		
+		if (a==b) 
+		{
+			System.out.println("A equal B");
+		}
+		else
+		{
+			System.out.println("No euqal");
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
